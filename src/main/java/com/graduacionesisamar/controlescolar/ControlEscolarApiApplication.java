8559b4f8143abdbb4e4d1f7e2b139ce3d017ba6f
Samuel Barrera Vera <sam.barrerav@hotmail.com>
2026-08-03 17:56:51 -0600

    Inicializa backend de Control Escolar con Spring Boot

---
+++ src/main/java/com/graduacionesisamar/controlescolar/ControlEscolarApiApplication.java
@@ -0,0 +1,13 @@
+package com.graduacionesisamar.controlescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlEscolarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlEscolarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graduacionesisamar/controlescolar/ControlEscolarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduacionesisamar.controlescolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlEscolarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
